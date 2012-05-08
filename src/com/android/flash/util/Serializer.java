5faeb9da54439aa62aaf3ee8bbf92229c5b4120d
John Wright <john.d.wright@gmail.com>
2012-05-07 23:25:38 -0700

    Full Json switch2 -Missed the actual json code inside serializer. Swapped it in.

--- src/com/android/flash/util/Serializer.java
+++ src/com/android/flash/util/Serializer.java
@@ -3,6 +3,8 @@ package com.android.flash.util;
 import android.os.Environment;
 import com.android.flash.SibOne;
 import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonParser;
 
 import java.io.*;
 import java.util.ArrayList;
@@ -10,35 +12,32 @@ import java.util.Calendar;
 
 /**
  * A static class used to persist SibOne data ArrayLists
- * 
+ *
  * @author johnwright
- * 
+ *
  */
 public class Serializer {
 
-	/**
-	 * Private constructor to prohibit any instances of this Serializer class
-	 */
-	private Serializer() {
+    /**
+     * Private constructor to prohibit any instances of this Serializer class
+     */
+    private Serializer() {
 
-	}
+    }
 
-	/**
-	 * Serialize an ArrayList<SibOne> myItems. Needs a FileOutputStream param.
-	 * Puts a backup in the external storage as well.
-	 *
+    /**
+     * Serialize an ArrayList<SibOne> myItems. Needs a FileOutputStream param.
+     * Puts a backup in the external storage as well.
+     *
      * @param objToSerialize
      *
      */
-	public static void serialize(ArrayList<SibOne> objToSerialize) {
-		if (!objToSerialize.isEmpty()) {
-			try {
-                /*java serialization
-				ObjectOutputStream os = new ObjectOutputStream(fos);
-				os.writeObject(objToSerialize);
-				os.close();
-                */
+    public static void serialize(ArrayList<SibOne> objToSerialize) {
+        if (!objToSerialize.isEmpty()) {
+            Gson gson = new Gson() ;
+            String json = gson.toJson(objToSerialize);
 
+            try {
                 //json serialization
                 String state = Environment.getExternalStorageState();
 
@@ -46,84 +45,70 @@ public class Serializer {
                     // We can read and write the media
                     File sdCard = Environment.getExternalStorageDirectory();
                     ObjectOutputStream os = new ObjectOutputStream(new FileOutputStream(sdCard.getAbsolutePath() + "/flashjson/"));
-                    os.writeObject(objToSerialize);
+                    os.writeObject(json);
+                    os.flush();
                     os.close();
                 }
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+            } catch (Exception e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
 
-			// if its monday, backup stuff to external
-			Calendar cal = Calendar.getInstance();
-			if (cal.get(Calendar.DAY_OF_WEEK) == Calendar.MONDAY) {
-				try {
-					String state = Environment.getExternalStorageState();
+            // if its monday, backup stuff to external
+            Calendar cal = Calendar.getInstance();
+            if (cal.get(Calendar.DAY_OF_WEEK) == Calendar.MONDAY) {
+                try {
+                    String state = Environment.getExternalStorageState();
 
-					if (Environment.MEDIA_MOUNTED.equals(state)) {
-						// We can read and write the media
-						File sdCard = Environment.getExternalStorageDirectory();
-                        /*java serialization
-						ObjectOutputStream os = new ObjectOutputStream(new FileOutputStream(sdCard.getAbsolutePath() + "/flash/"));
-						os.writeObject(objToSerialize);
-						os.close();
-						*/
-
-                        //json serialization
+                    if (Environment.MEDIA_MOUNTED.equals(state)) {
+                        File sdCard = Environment.getExternalStorageDirectory();
                         ObjectOutputStream os = new ObjectOutputStream(new FileOutputStream(sdCard.getAbsolutePath() + "/flashjsonbackup/"));
-                        os.writeObject(objToSerialize);
+                        os.writeObject(json);
+                        os.flush();
                         os.close();
-					} else {
-						System.out
-								.println("Failed to save to SDcard: Not writeable.");
-					}
-				} catch (Exception e) {
-					System.out.println("Failed to save to SDcard: Exception");
-				}
-			}
-		}
-	}
+                    } else {
+                        System.out
+                                .println("Failed to save to SDcard: Not writeable.");
+                    }
+                } catch (Exception e) {
+                    System.out.println("Failed to save to SDcard: Exception");
+                }
+            }
+        }
+    }
 
-	/**
-	 * Deserializes an ArrayList<SibOne> myItems. Needs a FileInputStream param.
-	 * 
-	 * @return
-	 */
-	@SuppressWarnings("unchecked")
-	public static ArrayList<SibOne> deserialize() {
-		ArrayList<SibOne> deserializedObject = null;
+    /**
+     * Deserializes an ArrayList<SibOne> myItems. Needs a FileInputStream param.
+     *
+     * @return
+     */
+    @SuppressWarnings("unchecked")
+    public static ArrayList<SibOne> deserialize() {
+        ArrayList<SibOne> deserializedObject = null;
 
-		try {
-            /*java deserialization
-			ObjectInputStream is = new ObjectInputStream(fis);
-			deserializedObject = (ArrayList<SibOne>) is.readObject();
-			is.close();
-            */
-
-            //json deserialization
+        try {
             String state = Environment.getExternalStorageState();
 
             if (Environment.MEDIA_MOUNTED.equals(state)) {
                 // We can read and write the media
                 File sdCard = Environment.getExternalStorageDirectory();
                 ObjectInputStream is = new ObjectInputStream(new FileInputStream(sdCard.getAbsolutePath() + "/flashjson/"));
-                deserializedObject = (ArrayList<SibOne>) is.readObject();
+                String json = (String) is.readObject();
                 is.close();
-            }
 
-		} catch (StreamCorruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (OptionalDataException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return deserializedObject;
-	}
+                JsonParser parser = new JsonParser();
+                JsonArray jsonArray = parser.parse(json).getAsJsonArray();
+
+                Gson gson = new Gson() ;
+                deserializedObject = new ArrayList<SibOne>();
+                for (int i=0; i<jsonArray.size(); i++) {
+                    deserializedObject.add(gson.fromJson(jsonArray.get(i), SibOne.class));
+
+                }
+            }
+        } catch (Exception e) {
+
+        }
+        return deserializedObject;
+    }
 }
